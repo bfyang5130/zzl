3fb6c50aaf592ae50469b8f4e9985ec6304fff25
Administrator <Administrator@bfyang5130>
2015-09-01 14:45:41 +0800

    顶部公共文件加入

--- themes/zuanzuanle/views/wechat/member/member_message.php
+++ themes/zuanzuanle/views/wechat/member/member_message.php
@@ -1,20 +1,5 @@
 <div class="page_content" style="-webkit-transform: translate3d(0px, 0px, 0px);">
-    <div class="col-lg-12" style="padding:10px 25px 0px 25px;">
-        <div class="panel panel-default" style="margin-bottom: 0px;">
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-xs-4">
-                        <img style="width:100px;height:100px;" class="img-rounded img-responsive" src="<?php echo Yii::app()->theme->baseUrl; ?>/images/wechat/huodong/product.jpg"/>
-                    </div>
-                    <div class="col-lg-8" style="text-align: left;">
-                        <p style="font-size: 20px;"><small>用户名：</small>bfyang5130</p>
-                        <p style="font-size:18px;"><small>可用资金：</small><font style="color:red;">50000</font></p>
-                        <p style="font-size:18px;"><small>会员级别：</small><font style="color:red;">初级会员</font></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php $this->renderPartial('//wechat/common/usertop') ?>
     <div class="col-lg-12" style="padding:10px 25px 0px 25px;min-height:350px;">
         <table class="table table-striped table-hover text-left" style="background-color:#f0f0f0;">
             <tr>
